9af7a5f1f21ffe896827047b7ed31b3ba09094b6
GregHib <greghib@users.noreply.github.com>
2023-10-11 16:47:25 +0100

    Add jdbc

--- push-to-neo4j.py
+++ push-to-neo4j.py
@@ -9,6 +9,10 @@ if __name__ == "__main__":
         .appName("push-to-neo4j") \
         .getOrCreate()
 
+    spark.read.jdbc("jdbc:postgresql://data-import-experiment-postgres.cgzrnuxjdpso.us-east-1.rds.amazonaws.com:5432",
+                    "northwind",
+                    properties={"user": "postgres", "password": "mypassword", "driver": "com.postgresql.Driver"})
+
     df = spark.createDataFrame([("John Doe", 32), ("Jane Doe", 42)], ["name", "age"])
 
     df.write.format("org.neo4j.spark.DataSource") \
